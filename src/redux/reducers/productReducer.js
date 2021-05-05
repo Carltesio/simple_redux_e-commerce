4780a0d138436e5eb436387d4afa54a331ac406a
carlos <carlos.delgado@hotmail.it>
2021-05-05 14:12:48 +0200

    cleaing files from react scaffold and setting up redux

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,11 @@
+import { ActionTypes } from "../contants/action-types";
+import initialState from "../store/initialState";
+
+export const productReducer = (state=initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+      default:
+        return state;
+  }
+};
---
+++ src/redux/store/initialState.js
@@ -0,0 +1,11 @@
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "Delgado",
+      category: "programmer",
+    },
+  ],
+};
+
+export default initialState
